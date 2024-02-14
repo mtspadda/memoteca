f7cfaca3b80fda5d119a89ad849d730934ac8507
Matheus Marques <matheuspadilha.dev@gmail.com>
2024-02-14 00:18:21 -0300

    novo: property binding

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id : '1',
+    conteudo: 'teste',
+    autoria: 'matheus',
+    modelo: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
